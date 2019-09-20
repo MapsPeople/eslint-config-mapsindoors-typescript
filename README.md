198f205adf982b0f60ae846bfa47a6db2cf7504c
Anders Mogensen <am@mapspeople.com>
2019-09-20 08:56:36 +0200

    Defining package and eslint rules.

---
+++ README.md
@@ -0,0 +1,5 @@
+# eslint-config-mapsindoors-typescript
+
+ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html) used in MapsIndoors TypeScript projects.
+
+Extends [eslint-config-mapsindoors](https://github.com/MapsPeople/eslint-config-mapsindoors)
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        "eslint-config-mapsindoors"
+    ],
+    rules: {
+        "@typescript-eslint/no-parameter-properties": 0,
+        "@typescript-eslint/explicit-member-accessibility": 0,
+        "@typescript-eslint/explicit-function-return-type": 1,
+    }
+};
