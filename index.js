94126d86495986769c3821c639fe277718637009
ammapspeople <50314165+ammapspeople@users.noreply.github.com>
2019-10-22 10:39:44 +0200

    Use the typescript implementation of no-unused-vars
    Otherwise, false positives are given.

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ module.exports = {
         "@typescript-eslint/no-parameter-properties": 0,
         "@typescript-eslint/explicit-member-accessibility": 0,
         "@typescript-eslint/explicit-function-return-type": 1,
+        "no-unused-vars": 0,
+        "@typescript-eslint/no-unused-vars": 2
     }
 };
