d9150b39ec7cd2b335f33aeac12076413dd330da
Mark Hougaard <mark@marks.dk>
2022-02-03 14:52:35 +0100

    fix: Add missing linter rules

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ module.exports = {
         }
     },
     rules: {
+        "no-unused-vars": 0,
+        "no-undef": 0,
         "@typescript-eslint/no-parameter-properties": "off",
         "@typescript-eslint/explicit-member-accessibility": "off", // Public or private
         "@typescript-eslint/explicit-function-return-type": "warn", // : Void as return type on functions without
         "@typescript-eslint/type-annotation-spacing": "warn", // Consistent spacing around type annotations
+        "@typescript-eslint/no-unused-vars": 2,
         "react/jsx-curly-spacing": ["error", "never"], // Disallow spaces inside of curly braces in JSX attributes
         "react/jsx-equals-spacing": ["error", "never"] // Disallow spaces around equal signs in JSX attributes
     }
